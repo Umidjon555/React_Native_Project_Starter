8f74aede8e44c50622dafe3002455c1dc47f2bd8
mac <mac@macui-MacBookPro.local>
2023-07-31 11:17:38 +0900

    Project Starter Folder

---
+++ utils/index.js
@@ -0,0 +1,6 @@
+// Export all the utils components
+export { Colors } from './Colors';
+export { TextStyles } from './TextStyles';
+
+export { Height, Width } from './Dimensions';
+// export { maskCPF, unmask } from './FormaterTexts';
